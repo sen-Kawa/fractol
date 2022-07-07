64e7a7309f30acad9d258985edea1d34d0d1c0bf
Karla Heinz <kaheinz@c2s14d80.42wolfsburg.de>
2022-07-07 14:39:40 +0200

    square in a different function

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 23:36:48 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/07/07 13:43:39 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/07/07 14:37:44 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 #include "mlx/mlx.h"
 
@@ -22,12 +23,51 @@ typedef struct s_data
     int     endian;
 }   t_data;
 
+void	circle(t_data *data);
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
+void	square(t_data *data, int line_x, int line_y);
+
 void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
     char    *dst;
 
     dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
+}
+
+void	square(t_data *data, int line_x, int line_y)
+{
+	while (line_x < 100)
+	{
+		if (line_x == 10 || line_x == 99)
+		{
+			while (line_y < 99)
+			{
+				my_mlx_pixel_put(data, line_x, line_y, 0x0000FFFF);
+				line_y++;
+			}
+		}
+		my_mlx_pixel_put(data, line_x, line_y, 0x0000FFFF);
+		line_x++;
+	}	
+	while (line_x > 10)
+	{
+		if (line_y == 10 || line_y == 99)
+		{
+			while (line_y > 10)
+			{
+				my_mlx_pixel_put(data, line_x, line_y, 0x0000FFFF);
+				line_y--;
+			}
+		}
+		my_mlx_pixel_put(data, line_x, line_y, 0x0000FFFF);
+		line_x--;
+	}
+}
+
+void	circle(t_data *data)
+{
+	my_mlx_pixel_put(data, 500, 500, 0x00FF0000);
 }
 
 int main(void)
@@ -44,32 +84,8 @@ int main(void)
     mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
     img.img = mlx_new_image(mlx, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while (line_x < 100)
-	{
-		if (line_x == 10 || line_x == 99)
-		{
-			while (line_y < 99)
-			{
-				my_mlx_pixel_put(&img, line_x, line_y, 0x0000FFFF);
-				line_y++;
-			}
-		}
-		my_mlx_pixel_put(&img, line_x, line_y, 0x0000FFFF);
-		line_x++;
-	}	
-	while (line_x > 10)
-	{
-		if (line_y == 10 || line_y == 99)
-		{
-			while (line_y > 10)
-			{
-				my_mlx_pixel_put(&img, line_x, line_y, 0x0000FFFF);
-				line_y--;
-			}
-		}
-		my_mlx_pixel_put(&img, line_x, line_y, 0x0000FFFF);
-		line_x--;
-	}	
+	square(&img, line_x, line_y);
+	circle(&img);
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
     return (0);
